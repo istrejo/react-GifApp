2907d0d3ac2fba717488e0b9ae40be5e940ce077
istrejo <istrejo2106@gmail.com>
2022-12-25 22:29:19 -0400

    GifGrid component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,34 +1,31 @@
 import React from 'react';
 import { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
 	const [categories, setCategories] = useState(['One Punch', 'DBZ']);
 
 	const onAddCategory = newCategory => {
 		console.log(newCategory);
+
+		if (categories.includes(newCategory)) return;
+
 		setCategories([newCategory, ...categories]);
 	};
 
 	return (
 		<>
-			{/* Title */}
-
 			<h1>GifExpertApp</h1>
 
-			{/* Input */}
 			<AddCategory
 				onNewCategory={value => onAddCategory(value)}
 				// setCategories={setCategories}
 			/>
 
-			{/* Gif List */}
-			<ol>
-				{categories.map((category, index) => (
-					<li key={index}>{category}</li>
-				))}
-			</ol>
-			{/* Gif Item */}
+			{categories.map(category => (
+				<GifGrid category={category} key={category} />
+			))}
 		</>
 	);
 };
